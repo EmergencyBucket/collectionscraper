b6b559beb8e618c1d69f9cca29f906afde36de05
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-02-22 16:22:50 -0500

    Concurrent??

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use futures::join;
+
 use api::make_bungie_request;
 
 pub mod api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let res = make_bungie_request("/Destiny2/Manifest/").await;
-    println!("{res:#?}");
+    let a = make_bungie_request("/Destiny2/Manifest/");
+    let b = make_bungie_request("/Destiny2/Manifest/");
+
+    let c = join!(a, b);
+
+    println!("{}", c.0);
+    println!("{}", c.1);
 
     Ok(())
 }
