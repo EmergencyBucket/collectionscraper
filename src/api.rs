b6be8ce271527582545af13eb253a82fd8bd5b45
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-02-22 12:01:15 -0500

    Start bungie requests

--- src/api.rs
+++ src/api.rs
@@ -1,3 +1,8 @@
+/// No changes can be made with this API key so it can be public
+const BUNGIE_KEY: &'static str = "529cac5f9e3a482b86b931f1f75f2331";
+
+/// Generates a random ipv6 address in the subnet  
+/// This subnet is specific to the eBucket server ```2a01:4f9:3051:4a65::/64```
 fn generate_address() -> std::net::IpAddr {
     std::net::IpAddr::from([
         0x2a01,
@@ -20,5 +25,26 @@ pub async fn make_request(url: &str) -> String {
         .unwrap();
 
     let res = client.get(url).send().await.unwrap().text().await.unwrap();
+    res
+}
+
+pub async fn make_bungie_request(path: &str) -> String {
+    let url = format!("https://www.bungie.net/Platform{}", path);
+    let addr = generate_address();
+
+    let client = reqwest::Client::builder()
+        .local_address(addr)
+        .build()
+        .unwrap();
+
+    let res = client
+        .get(&url)
+        .header("X-API-Key", BUNGIE_KEY)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
     res
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use api::make_request;
+use api::make_bungie_request;
 
 pub mod api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let res = make_request("https://api64.ipify.org?format=text").await;
+    let res = make_bungie_request("/Destiny2/Manifest/").await;
     println!("{res:#?}");
 
     Ok(())
