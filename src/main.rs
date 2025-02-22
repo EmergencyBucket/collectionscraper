f42209dbe052e26014b838597e19d1cc0764fec7
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-02-22 02:40:09 -0500

    Random address instead

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use std::net::IpAddr;
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>  {
-    let proxy = reqwest::Proxy::all("http://0.0.0.0:51080")?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = IpAddr::from([
+        0x2a01,
+        0x4f9,
+        0x3051,
+        0x4a65,
+        rand::random::<u16>(),
+        rand::random::<u16>(),
+        rand::random::<u16>(),
+        rand::random::<u16>(),
+    ]);
 
-    let client = reqwest::Client::builder().proxy(proxy).build()?;
+    let client = reqwest::Client::builder().local_address(addr).build()?;
 
-    let res = client.get("http://ipv6.ip.sb").send()
-        .await?
-        .text()
-        .await?;
+    let res = client.get("http://ipv6.ip.sb").send().await?.text().await?;
     println!("{res:#?}");
 
     Ok(())
