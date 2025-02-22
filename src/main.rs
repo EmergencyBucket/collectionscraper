d43f80de8acae392347111fbde0228e6eb78f2bf
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-02-22 02:02:53 -0500

    Extract proxy

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>  {
-    let client = reqwest::Client::builder().proxy(reqwest::Proxy::all("http://127.0.0.1:51080").unwrap()).build().unwrap();
+    let proxy = reqwest::Proxy::all("http://0.0.0.0:51080")?;
+
+    let client = reqwest::Client::builder().proxy(proxy).build()?;
 
     let res = client.get("http://httpbin.org/ip").send()
         .await?
