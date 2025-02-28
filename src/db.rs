f0f0dbab65ebb297c13af8141f6220c556ccb53e
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-02-27 19:51:58 -0500

    c

--- src/db.rs
+++ src/db.rs
@@ -12,6 +12,8 @@ pub fn get_client() -> Client {
         .with_url(read_env_var("CLICKHOUSE_URL"))
         .with_user(read_env_var("CLICKHOUSE_USER"))
         .with_password(read_env_var("CLICKHOUSE_PASSWORD"))
+        .with_option("async_insert", "1")
+        .with_option("wait_for_async_insert", "0")
 }
 
 #[derive(Row, Serialize)]
@@ -31,7 +33,11 @@ pub async fn push_data(data: Vec<UsersRow>) {
 
     let mut insert = client
         .insert::<UsersRow>("users_v3")
-        .unwrap().with_timeouts(Some(Duration::from_secs(1_000_000)), Some(Duration::from_secs(1_000_000)));
+        .unwrap()
+        .with_timeouts(
+            Some(Duration::from_secs(1_000_000)),
+            Some(Duration::from_secs(1_000_000)),
+        );
 
     for row in data {
         if row.emblems.len() == 0 {
